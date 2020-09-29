47f00d0e356a078dce485647cf1039af2f53fc92
Mohammad Farhadi <Fsunroo@gmail.com>
2020-09-29 07:19:42 -0700

    Update PowerVextract.py

--- PowerVextract.py
+++ PowerVextract.py
@@ -1,8 +1,20 @@
 from zipfile import ZipFile
 import os
+from moviepy.editor import *
+
 POWERLIST= []
 
-for file in os.listdir():
+
+def sort(path):
+	for file in os.listdir(path):
+		if file[6] == '.':
+			new_file = file[:5]+'0'+file[5:]
+			os.rename(path+'\\'+file,path+'\\'+new_file)
+
+
+
+
+for file in os.listdir():          #convert All files to zip
 	if file.endswith('.ppt'):
 		file=file.replace('.ppt','')
 		os.rename(file+'.ppt',file+'.zip')
@@ -19,17 +31,16 @@ for power in POWERLIST:
 	with ZipFile(power,'r') as zipObj:
 		for content in zipObj.namelist():
 			if content.endswith('.wma'):
-				zipObj.extract(content,'voice')
+				zipObj.extract(content,f'{power}-voice')
 
-	segments_path= os.path.join('voice','ppt','media')
-	voice_path = os.path.join('voice',power.replace('.zip','.mp3'))
-	with open(voice_path,'wb') as voice:
-		datas=bytes()
-		for segment in os.listdir(segments_path):
-			data = open(f'{segments_path}//{segment}','rb')
-			datas= datas + data.read()
-			print(data==datas)
-		voice.write(datas)
-	voice.close()
-
-		
+	segments_path= os.path.join(f'{power}-voice','ppt','media')
+	voice_name = power.replace('.zip','.mp3')
+	segments = []
+	sort(segments_path)
+	for segment in os.listdir(segments_path) :
+		if segment.endswith('.wma'):
+			segments.append(AudioFileClip(os.path.join(segments_path ,segment)))
+	audioClips = concatenate_audioclips([segment for segment in segments])
+	audioClips.write_audiofile(voice_name)
+	os.rmdir(f'{power}-voice')
+	os.rename(power ,power.replace('.zip', '.pptx'))

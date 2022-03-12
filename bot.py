38b702359546a311872160258849ff612380b981
mohammad farhadi <fsunroo@gmail.com>
2022-03-12 21:23:24 +0330

    update

--- bot.py
+++ bot.py
@@ -4,17 +4,23 @@ import time
 
 
 Token = open('token', 'r').read().strip()
+print(Token)
 bot = telebot.TeleBot(Token)
+print('RUNNING...')
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    print('start')
     bot.send_message(message.chat.id, 'Hello, ' + message.from_user.first_name + '!')
 
 @bot.message_handler(content_types=['document'])
 def extractvoice(message):
-    PowerVextract.VoiceExtract(message.document.file_id)
-    bot.send_message(message.chat.id, 'Done!')
-
+    bot.reply_to(message, 'downloading...')
+    file_info = bot.get_file(message.document.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    PowerVextract.VoiceExtract(downloaded_file)
+    bot.reply_to(message, 'done!')
+    bot.send_media_group(message.chat.id, ['media.mp3'])
 
 while True:
     try:
